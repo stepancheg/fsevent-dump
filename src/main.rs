cd5c884f86e60bf9ebfaeb71e287e191e9cd33eb
Stiopa Koltsov <stepan.koltsov@gmail.com>
2022-02-05 08:29:45 +0000

    backup

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
+use std::env;
 use std::path::Path;
 
 fn main() {
+    let args = env::args().collect::<Vec<_>>();
+    let path = match args.as_slice() {
+        [_, path] => path,
+        _ => panic!("Usage: {} <path>", env::args().next().unwrap()),
+    };
+
     // Automatically select the best implementation for your platform.
     let mut watcher = listen_notify::FsEventWatcher::new();
 
     // Add a path to be watched. All files and directories at that path and
     // below will be monitored for changes.
-    watcher.watch(Path::new("/Users/nga/fbsource"));
+    watcher.watch(Path::new(path));
 
     unreachable!();
 }
